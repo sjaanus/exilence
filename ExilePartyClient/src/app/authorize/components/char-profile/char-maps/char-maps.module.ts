82bdad94e8cc5c2b2b77a6c26046222c2c71c86f
Petter Annerwall <petterannerwall@gmail.com>
2018-11-18 12:27:23 +0100

    Added missing modules from merge

--- ExilePartyClient/src/app/authorize/components/char-profile/char-maps/char-maps.module.ts
+++ ExilePartyClient/src/app/authorize/components/char-profile/char-maps/char-maps.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatInputModule, MatMenuModule } from '@angular/material';
 
 import { SharedModule } from '../../../../shared/shared.module';
 import { InfoDialogComponent } from '../../info-dialog/info-dialog.component';
@@ -10,6 +10,8 @@ import { CharMapsComponent } from './char-maps.component';
 
 @NgModule({
     imports: [
+        MatMenuModule,
+        MatIconModule,
         SharedModule,
         MapTableModule,
         FormsModule,
